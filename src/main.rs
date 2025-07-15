a65d176eb6ef834f2b2facdea1c3b9703c7bdcf9
sudo-pacman-R-skill-issue <iamtugarin@gmail.com>
2025-07-15 12:22:54 +0300

    пока живём, пока бебрим, пока легко

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,30 @@
 #![allow(unused_imports)]
-use std::{io::Write, net::TcpListener};
+use std::{io::{BufReader, Error, Read, Write}, net::TcpListener};
 
-fn main() {
+fn main() -> Result<(), Error> {
     println!("Logs from your program will appear here!");
 
     
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
+    let mut buf = [0u8;512];
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                stream.write_all(b"+PONG\r\n").unwrap();
-                println!("accepted new connection");
+            Ok(stream) => {
+                let mut buf_reader = BufReader::new(stream);
+                loop {
+                    if buf_reader.read(&mut buf).unwrap() > 0 {
+                        let streamchik = buf_reader.get_mut();
+                        streamchik.write_all(b"+PONG\r\n")?;
+                        streamchik.flush()?;
+                    } else {
+                        break
+                    }
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+    Ok(())
 }
