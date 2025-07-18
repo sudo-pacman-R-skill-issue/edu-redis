c2b019dec85cac36b1d1b9b22ec12b0912eb9d04
sudo-pacman-R-skill-issue <iamtugarin@gmail.com>
2025-07-18 18:13:44 +0300

    add todo comments

--- src/handler.rs
+++ src/handler.rs
@@ -15,42 +15,32 @@ fn combine_bytes_vector(vec_bytes: Vec<Bytes>) -> Bytes {
     buffer.freeze()
 }
 
-pub fn extract_value(resp: RespOrig) -> Option<Bytes> {
-    match resp {
-        RespOrig::String(bytes) => Some(bytes),
-        RespOrig::Error(bytes) => Some(bytes),
-        RespOrig::Int(int) => Some(Bytes::copy_from_slice(format!("{}", int).as_bytes())),
-        RespOrig::Array(resp_origs) => {
-            let mut result = Vec::with_capacity(resp_origs.len());
-            resp_origs
-                .into_iter()
-                .for_each(|element| 
-                    match extract_value(element) {
-                        Some(value) => {
-                            result.push(append_crlf(&value));
-                        }
+impl RespOrig {
+    /// TODO лишняя функция, лучше сразу обрабатывать входящие команды(назвать функцию handle_command)
+    /// TODO а то получается лишняя обёртка
+    pub fn extract_value(self) -> Option<Bytes> {
+        match self {
+            RespOrig::String(bytes) => Some(bytes),
+            RespOrig::Error(bytes) => Some(bytes),
+            RespOrig::Int(int) => Some(Bytes::copy_from_slice(format!("{}", int).as_bytes())),
+            RespOrig::Array(resp_origs) => {
+                let mut result = Vec::with_capacity(resp_origs.len());
+                resp_origs.into_iter().for_each(|element| 
+                    match Self::extract_value(element) {
+                        Some(value) => result.push(append_crlf(&value)),
                         None => todo!(),
                     }
                 );
-            let res = combine_bytes_vector(result);
-            Some(res)
-        }
-        RespOrig::NullArray => None,
-        RespOrig::NullBulkString => None,
-    }
-}
-
-impl RespOrig {
-    fn handler(&self) -> Bytes {
-        match self {
-            RespOrig::String(bytes) => todo!(),
-            RespOrig::Error(bytes) => todo!(),
-            RespOrig::Int(_) => todo!(),
-            RespOrig::Array(resp_origs) => {
-                todo!()
+                let res = combine_bytes_vector(result);
+                Some(res)
             }
-            RespOrig::NullArray => todo!(),
-            RespOrig::NullBulkString => todo!(),
+            RespOrig::NullArray => None,
+            RespOrig::NullBulkString => None,
         }
+    }
+
+    ///TODO: функция энкодер в resp протокол
+    pub fn to_resp(value: Bytes) {
+        todo!()
     }
 }
